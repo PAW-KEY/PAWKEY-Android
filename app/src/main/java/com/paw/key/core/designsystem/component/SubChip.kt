c702e2a669e6d40ffb3ced5e8aa8facc7f4ea8f1
Son Juwan <wndhks171@naver.com>
2025-07-07 22:24:50 +0900

    feat/#18 : 공통 컴포넌트 수정

--- app/src/main/java/com/paw/key/core/designsystem/component/SubChip.kt
+++ app/src/main/java/com/paw/key/core/designsystem/component/SubChip.kt
@@ -36,7 +36,7 @@ fun SubChip(
         Text(
             text = text,
             color = PawKeyTheme.colors.gray300,
-            style = PawKeyTheme.typography.body7R13
+            style = PawKeyTheme.typography.body14M
         )
     }
 
