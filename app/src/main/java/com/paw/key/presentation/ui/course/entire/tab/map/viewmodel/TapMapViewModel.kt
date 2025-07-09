9288cfccb29ad92a941595687fc75e1970b5d749
sonms <smsms5676@naver.com>
2025-07-09 12:16:15 +0900

    feat/#11: 지도 탭 contract, viewmodel 구현

---
+++ app/src/main/java/com/paw/key/presentation/ui/course/entire/tab/map/viewmodel/TapMapViewModel.kt
@@ -0,0 +1,39 @@
+package com.paw.key.presentation.ui.course.entire.tab.map.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.kakao.vectormap.LatLng
+import com.paw.key.core.util.UiState
+import com.paw.key.presentation.ui.course.entire.tab.map.state.TapMapContract.TapMapState
+import com.paw.key.presentation.ui.course.entire.tab.map.state.TapMapContract.TapMapSideEffect
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class TapMapViewModel @Inject constructor(
+
+) : ViewModel() {
+    private val _state = MutableStateFlow(TapMapState())
+    val state : StateFlow<TapMapState>
+        get() = _state.asStateFlow()
+
+    private val _sideEffect = MutableSharedFlow<TapMapSideEffect>()
+    val sideEffect: MutableSharedFlow<TapMapSideEffect>
+        get() = _sideEffect
+
+    fun updateInitialLocationState(newState: UiState<LatLng>) {
+        _state.value = _state.value.copy(
+            initialLocationState = newState
+        )
+    }
+
+    fun updateState(reducer: TapMapState.() -> TapMapState) {
+        _state.update {
+            it.reducer()
+        }
+    }
+}
