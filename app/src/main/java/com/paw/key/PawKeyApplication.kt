2b3f0fd74073f78018556895e102bbbfac3df649
sonms <smsms5676@naver.com>
2025-07-09 12:44:11 +0900

    chore/#11: Application 수준에서 카카오 sdk

--- app/src/main/java/com/paw/key/PawKeyApplication.kt
+++ app/src/main/java/com/paw/key/PawKeyApplication.kt
@@ -2,16 +2,25 @@ package com.paw.key
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.kakao.vectormap.KakaoMapSdk
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
+import javax.inject.Named
 
 @HiltAndroidApp
 class PawKeyApplication : Application() {
+    @Inject
+    @Named("kakao.native.key")
+    lateinit var kakaoNativeKey: String
+
     override fun onCreate() {
         super.onCreate()
 
         setTimber()
         setDarkMode()
+
+        KakaoMapSdk.init(this, kakaoNativeKey)
     }
 
     private fun setTimber() {
