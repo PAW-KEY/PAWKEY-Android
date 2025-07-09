f7a975b004a1df297f544269032a9ed7045a247d
sonms <smsms5676@naver.com>
2025-07-09 12:36:09 +0900

    chore/#11: 메인 바텀 visible 수정

--- app/src/main/java/com/paw/key/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/paw/key/presentation/ui/main/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.paw.key.presentation.ui.main
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.core.view.WindowCompat
 import com.paw.key.core.designsystem.theme.PawKeyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/paw/key/presentation/ui/main/component/MainBottomBar.kt
+++ app/src/main/java/com/paw/key/presentation/ui/main/component/MainBottomBar.kt
@@ -43,7 +43,7 @@ fun MainBottomBar(
     modifier: Modifier = Modifier,
 ) {
     AnimatedVisibility (
-        visible = true,
+        visible = isVisible,
         enter = EnterTransition.None,
         exit = ExitTransition.None,
         modifier = modifier
@@ -56,7 +56,7 @@ fun MainBottomBar(
             contentAlignment = Alignment.Center
         ) {
             Surface(
-                color = Color.Black,
+                color = PawKeyTheme.colors.gray950,
                 shape = RoundedCornerShape(200.dp),
                 shadowElevation = 10.dp,
                 modifier = Modifier
