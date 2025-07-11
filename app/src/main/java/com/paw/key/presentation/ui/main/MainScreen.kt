f0b8d86695c37e0103946a18780d625556f67697
sonms <smsms5676@naver.com>
2025-07-11 23:06:46 +0900

    feat/#45: MainScreen 애니메이션에 맞춰 구현

--- app/src/main/java/com/paw/key/presentation/ui/main/MainScreen.kt
+++ app/src/main/java/com/paw/key/presentation/ui/main/MainScreen.kt
@@ -2,6 +2,7 @@ package com.paw.key.presentation.ui.main
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
@@ -13,16 +14,39 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.paw.key.presentation.animation.FootprintAnimationScreen
 import com.paw.key.presentation.ui.main.component.MainBottomBar
+import com.paw.key.presentation.ui.main.state.MainContract
+import com.paw.key.presentation.ui.main.viewmodel.MainViewModel
 import kotlinx.collections.immutable.toImmutableList
 
+@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
+@Composable
+fun MainRoute(
+    viewModel: MainViewModel = hiltViewModel(),
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    MainScreen(
+        footprints = state.footprint,
+        addFootprint = viewModel::addFootprint,
+        removeFootprint = viewModel::removeFootprint
+    )
+}
 
 @RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
 @Composable
 fun MainScreen(
+    footprints: List<MainContract.Footprint>,
+    addFootprint: (MainContract.Footprint) -> Unit,
+    removeFootprint: (MainContract.Footprint) -> Unit,
     navigator: MainNavigator = rememberMainNavigator(),
 ) {
     val snackBarHostState = remember { SnackbarHostState() }
@@ -30,6 +54,13 @@ fun MainScreen(
     MainScreenContent(
         navigator = navigator,
         snackBarHostState = snackBarHostState,
+        footprints = footprints,
+        addFootprint = { footprint ->
+            addFootprint(footprint)
+        },
+        removeFootprint = { footprint ->
+            removeFootprint(footprint)
+        }
     )
 }
 
@@ -38,19 +69,25 @@ fun MainScreen(
 private fun MainScreenContent(
     navigator: MainNavigator,
     snackBarHostState: SnackbarHostState,
-    modifier: Modifier = Modifier
+    footprints: List<MainContract.Footprint>,
+    addFootprint: (MainContract.Footprint) -> Unit,
+    removeFootprint: (MainContract.Footprint) -> Unit,
 ) {
     Box(
-        modifier = modifier
+        modifier = Modifier
             .windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Top))
             .systemBarsPadding()
             .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTapGestures { offset ->
+                    addFootprint(MainContract.Footprint(position = offset))
+                }
+            }
     ) {
         PawKeyNavHost(
             navigator = navigator,
             paddingValues = PaddingValues(),
-            snackbarHostState = snackBarHostState,
-            modifier = modifier
+            snackbarHostState = snackBarHostState
         )
 
         MainBottomBar(
@@ -58,8 +95,16 @@ private fun MainScreenContent(
             tabs = MainTab.entries.toImmutableList(),
             currentTab = navigator.currentTab,
             onTabSelected = navigator::navigate,
-            modifier = modifier
-                .align(Alignment.BottomCenter)
+            modifier = Modifier.align(Alignment.BottomCenter)
+        )
+
+        FootprintAnimationScreen(
+            footprints = footprints,
+            onAnimationFinished = { finishedFootprint ->
+                removeFootprint(finishedFootprint)
+            },
+            modifier = Modifier
+                .fillMaxSize()
         )
     }
 }
