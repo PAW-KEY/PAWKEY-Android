33d3d76a6a2244f76d3003788b697ff91e945864
Son Juwan <wndhks171@naver.com>
2025-07-11 19:20:59 +0900

    feat/#35: onboarding_signup viewmodel,state

---
+++ app/src/main/java/com/paw/key/presentation/ui/signup/state/SignUpContract.kt
@@ -0,0 +1,50 @@
+package com.paw.key.presentation.ui.signup.state
+
+import androidx.compose.runtime.Immutable
+
+class SignUpContract {
+    @Immutable
+    data class SignUpState(
+        val selectedGender: Gender = Gender.UNKNOWN,
+        val selectedLocation: String = "",
+        val isLocationMenuVisible: Boolean = false,
+        val name: String = "",
+        val age: String = "",
+
+        val dogName: String = "",
+        val dogGender: DogGender = DogGender.UNKNOWN,
+        val isNeutered: Boolean = false,
+        val dogBreed: String = "",
+        val ageKnown: AgeKnown = AgeKnown.UNKNOWN,
+        val dogAge: String = "",
+
+        val selectedEnergyLevel: String = "",
+        val selectedSocialLevel: String = "",
+
+        val selectedDistrict: List<String> = emptyList(),
+        val isDistrictMenuVisible: Boolean = false,
+    )
+
+    enum class Gender {
+        MALE,
+        FEMALE,
+        UNKNOWN
+    }
+
+    enum class DogGender {
+        MALE,
+        FEMALE,
+        UNKNOWN
+    }
+
+    enum class AgeKnown {
+        KNOWN,
+        UNKNOWN
+    }
+
+    sealed class SignUpSideEffect {
+        data class ShowSnackBar(val message: String) : SignUpSideEffect()
+        data object NavigateUp: SignUpSideEffect()
+        data object NavigateNext: SignUpSideEffect()
+    }
+}
