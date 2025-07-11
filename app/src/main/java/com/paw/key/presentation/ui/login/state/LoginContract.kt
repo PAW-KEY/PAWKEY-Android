b118cdc60830a6521495ff234d9ca3ad59f89f52
sonms <smsms5676@naver.com>
2025-07-11 09:40:17 +0900

    feat/#42: 로그인 상태

---
+++ app/src/main/java/com/paw/key/presentation/ui/login/state/LoginContract.kt
@@ -0,0 +1,22 @@
+package com.paw.key.presentation.ui.login.state
+
+import androidx.compose.runtime.Immutable
+
+class LoginContract {
+    @Immutable
+    data class LoginState(
+        val email: String = "",
+        val password: String = "",
+
+        val isPasswordVisible: Boolean = false
+    )
+
+    sealed class LoginSideEffect {
+        data class ShowSnackBar(val message: String) : LoginSideEffect()
+        data object NavigateUp: LoginSideEffect()
+        data object NavigateNext: LoginSideEffect()
+
+        data object SignInSucceed : LoginSideEffect()
+        data object SignInFailed : LoginSideEffect()
+    }
+}
