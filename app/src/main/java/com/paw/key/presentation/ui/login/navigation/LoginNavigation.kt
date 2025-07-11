128b38bdfe5dac6fc3a4ffa57517cebc079ac25e
sonms <smsms5676@naver.com>
2025-07-11 09:48:59 +0900

    feat/#42: 로그인 네비게이션 구현

--- app/src/main/java/com/paw/key/presentation/ui/login/navigation/LoginNavigation.kt
+++ app/src/main/java/com/paw/key/presentation/ui/login/navigation/LoginNavigation.kt
@@ -1,8 +1,9 @@
 package com.paw.key.presentation.ui.login.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
@@ -12,27 +13,24 @@ import com.paw.key.presentation.ui.login.LoginRoute
 import kotlinx.serialization.Serializable
 
 fun NavController.navigateLogin(
-    navOptions: NavOptions?,
+    navOptions: NavOptions?
 ) {
     navigate(Login, navOptions)
 }
 
+@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
 fun NavGraphBuilder.loginNavGraph(
     paddingValues: PaddingValues,
     navigateUp: () -> Unit,
     navigateNext: () -> Unit,
-    navigateSignUp: () -> Unit,
     snackBarHostState: SnackbarHostState,
-    modifier: Modifier = Modifier,
 ) {
     composable<Login> {
         LoginRoute(
-            paddingValues = paddingValues,
             navigateUp = navigateUp,
             navigateNext = navigateNext,
-            navigateSignUp = navigateSignUp,
             snackBarHostState = snackBarHostState,
-            modifier = modifier
+            paddingValues = paddingValues
         )
     }
 }
