44e0d0478831e47a7a5848b5e3a44701bce6517a
sonms <smsms5676@naver.com>
2025-07-13 16:29:37 +0900

    feat/#50: 지도 상태값 공유 레포 구현

---
+++ app/src/main/java/com/paw/key/domain/repository/WalkSharedResultRepository.kt
@@ -0,0 +1,12 @@
+package com.paw.key.domain.repository
+
+import android.graphics.Bitmap
+import com.kakao.vectormap.LatLng
+import com.paw.key.domain.model.entity.sharedresult.WalkResult
+import kotlinx.coroutines.flow.Flow
+
+interface WalkSharedResultRepository {
+    suspend fun saveResult(bitmap: Bitmap?, totalTime: Long, distance: Float, steps: Int, points : List<LatLng>)
+    fun getResult(): Flow<WalkResult?>
+    fun clear()
+}
