2a14e1c6dd1938fe06e6d9dcae90c91973a259db
Son Juwan <wndhks171@naver.com>
2025-07-14 07:17:06 +0900

    mod/#64: 공통 컴포넌트 수정

--- app/src/main/java/com/paw/key/core/designsystem/component/PawkeyButton.kt
+++ app/src/main/java/com/paw/key/core/designsystem/component/PawkeyButton.kt
@@ -1,23 +1,18 @@
 package com.paw.key.core.designsystem.component
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.paw.key.core.designsystem.theme.PawKeyTheme
@@ -28,15 +23,15 @@ import com.paw.key.core.util.noRippleClickable
 @Composable
 private fun PreviewPawkeyButton() {
     PawKeyTheme {
-        Column {
+        Column (
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+        ){
             // 초록색 버튼
             PawkeyButton(
                 text = "신규 계정으로 회원가입",
                 enabled = true,
                 onClick = {}
             )
-
-            Spacer(modifier = Modifier.height(12.dp))
 
             // 회색 버튼
             PawkeyButton(
@@ -50,6 +45,7 @@ private fun PreviewPawkeyButton() {
                 text = "신규 계정으로 회원가입",
                 enabled = true,
                 onClick = {},
+                isBorder = false,
                 isBackGround = true
             )
 
@@ -99,27 +95,23 @@ fun PawkeyButton(
     val borderColor = when {
         enabled && isBackGround -> PawKeyTheme.colors.green500
         !enabled && isBackGround -> PawKeyTheme.colors.gray200
-        else -> PawKeyTheme.colors.white1
+        else -> Color.Transparent
     }
 
-    Button(
-        onClick = onClick,
-        enabled = enabled,
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(8.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = backgroundColor,
-            contentColor = contentColor,
-            disabledContainerColor = backgroundColor,
-            disabledContentColor = contentColor
-        ),
-        border = if (isBackGround) BorderStroke(3.dp, borderColor) else null
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(backgroundColor, shape = RoundedCornerShape(8.dp))
+            .then(if (isBorder || isBackGround) Modifier.border(1.dp, borderColor, RoundedCornerShape(8.dp)) else Modifier)
+            .noRippleClickable{ onClick() }
+            .padding(vertical = 14.dp),
+        contentAlignment = Alignment.Center
     ) {
         Text(
             text = text,
-            modifier = Modifier
-                .padding(vertical = 14.dp),
-            style = PawKeyTheme.typography.body16Sb
+            style = PawKeyTheme.typography.body16Sb,
+            color = contentColor
         )
     }
 }
+
