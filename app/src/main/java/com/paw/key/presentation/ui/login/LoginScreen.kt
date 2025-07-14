73d8b08344707a0aea5f804a481ab623a80378e8
Son Juwan <wndhks171@naver.com>
2025-07-15 05:54:15 +0900

    mod/#70: 로그인 UI, viewmodel 수정

--- app/src/main/java/com/paw/key/presentation/ui/login/LoginScreen.kt
+++ app/src/main/java/com/paw/key/presentation/ui/login/LoginScreen.kt
@@ -1,7 +1,6 @@
 package com.paw.key.presentation.ui.login
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -19,7 +18,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +28,6 @@ import com.paw.key.R
 import com.paw.key.core.designsystem.component.PawkeyButton
 import com.paw.key.core.designsystem.component.TopBar
 import com.paw.key.core.designsystem.theme.PawKeyTheme
-import com.paw.key.core.util.isKeyboardOpen
 import com.paw.key.core.util.noRippleClickable
 import com.paw.key.presentation.ui.login.component.LoginTextField
 import com.paw.key.presentation.ui.login.viewmodel.LoginViewModel
