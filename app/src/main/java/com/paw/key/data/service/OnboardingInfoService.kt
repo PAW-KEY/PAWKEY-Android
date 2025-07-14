f0a9f4c2c9639c6e602533191498a99aa44e72c9
Son Juwan <wndhks171@naver.com>
2025-07-15 05:55:03 +0900

    mod/#70: 온보딩 유저 정보 요청 repository,service 추가

---
+++ app/src/main/java/com/paw/key/data/service/OnboardingInfoService.kt
@@ -0,0 +1,22 @@
+package com.paw.key.data.service
+
+import com.paw.key.data.dto.request.onboarding.OnboardingInfoRequest
+import com.paw.key.data.dto.response.BaseResponse
+import com.paw.key.data.dto.response.onboarding.OnboardingInfoResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface OnboardingInfoService {
+    @Multipart
+    @POST("users")
+    suspend fun postInfo(
+        @Header("X-USER-ID") userId: Int,
+        @Part("data") data: RequestBody,
+        @Part petProfile: MultipartBody.Part
+    ): OnboardingInfoResponse
+}
