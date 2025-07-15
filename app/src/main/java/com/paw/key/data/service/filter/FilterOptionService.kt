2bf67a91e695d3676710901e849a4bf86adce2a5
Son Juwan <wndhks171@naver.com>
2025-07-15 21:52:24 +0900

    feat/#82: filterOption module service, repository 추가

---
+++ app/src/main/java/com/paw/key/data/service/filter/FilterOptionService.kt
@@ -0,0 +1,14 @@
+package com.paw.key.data.service.filter
+
+import com.paw.key.data.dto.response.BaseResponse
+import com.paw.key.data.dto.response.filter.FilterOptionResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+
+interface FilterOptionService {
+    @GET ("posts/categories")
+    suspend fun getFilterOptions(
+        @Header("X-USER-ID") userId: Int,
+    ): BaseResponse<FilterOptionResponse>
+}
