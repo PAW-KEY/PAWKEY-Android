f85a52b5f366a9bddb21a93b12213b165d05b7e2
Son Juwan <wndhks171@naver.com>
2025-07-16 09:05:09 +0900

    feat/#92: PostList service

---
+++ app/src/main/java/com/paw/key/domain/repository/list/PostsListRepository.kt
@@ -0,0 +1,9 @@
+package com.paw.key.domain.repository.list
+
+import com.paw.key.data.dto.request.list.PostsListRequestDto
+import com.paw.key.domain.model.entity.list.ListEntity
+
+interface PostsListRepository {
+    suspend fun postList(userId: Int, request: PostsListRequestDto): Result<ListEntity>
+    suspend fun getAllPosts(userId: Int): Result<ListEntity>
+}
