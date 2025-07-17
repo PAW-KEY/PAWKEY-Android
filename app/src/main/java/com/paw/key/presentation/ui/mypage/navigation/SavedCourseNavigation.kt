fe463be0637652bb79770c41437763db1dd4fb1e
sonms <smsms5676@naver.com>
2025-07-17 22:40:48 +0900

    feat/#120: 저장 코스와 저장 상세보기 네비게이션 연결

--- app/src/main/java/com/paw/key/presentation/ui/mypage/navigation/SavedCourseNavigation.kt
+++ app/src/main/java/com/paw/key/presentation/ui/mypage/navigation/SavedCourseNavigation.kt
@@ -8,12 +8,13 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import com.paw.key.core.navigation.Route
 import com.paw.key.presentation.ui.mypage.SavedCourseRoute
 import kotlinx.serialization.Serializable
 
 fun NavController.navigateSavedCourse(
-    navOptions: NavOptions?
+    navOptions: NavOptions?,
 ) {
     navigate(SavedCourse, navOptions)
 }
@@ -21,14 +22,18 @@ fun NavController.navigateSavedCourse(
 fun NavGraphBuilder.savedCourseNavGraph(
     paddingValues: PaddingValues,
     navigateUp: () -> Unit,
-    navigateNext: () -> Unit,
+    navigateNext: (Int, Int) -> Unit,
     snackBarHostState: SnackbarHostState,
     modifier: Modifier = Modifier,
 ) {
-    composable<SavedCourse> {
+    composable<SavedCourse> { backStackEntry ->
+        val ids = backStackEntry.toRoute<SavedCourse>()
+
         SavedCourseRoute(
             navigateUp = navigateUp,
-            navigateNext = navigateNext,
+            navigateNext = { routeId, pageId ->
+                navigateNext(routeId, pageId)
+            },
             modifier = modifier
         )
     }
