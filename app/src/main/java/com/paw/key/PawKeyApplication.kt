930c07098deb04a6bd238b04a575b7166d1c5dba
sonms <smsms5676@naver.com>
2025-09-21 17:37:40 +0900

    feat/#137 coil 이미지 로더 팩토리 적용 - 이미지 캐싱

--- app/src/main/java/com/paw/key/PawKeyApplication.kt
+++ app/src/main/java/com/paw/key/PawKeyApplication.kt
@@ -2,6 +2,11 @@ package com.paw.key
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.util.DebugLogger
 import com.kakao.vectormap.KakaoMapSdk
 import com.naver.maps.map.NaverMapSdk
 import dagger.hilt.android.HiltAndroidApp
@@ -10,7 +15,7 @@ import javax.inject.Inject
 import javax.inject.Named
 
 @HiltAndroidApp
-class PawKeyApplication : Application() {
+class PawKeyApplication : Application(), ImageLoaderFactory {
     @Inject
     @Named("kakao.native.key")
     lateinit var kakaoNativeKey: String // BuildConfig는 컴파일 타임에 생성되는 정적 클래스이기 때문에 Mocking이 불가능 = 테스트 용이성
@@ -32,5 +37,23 @@ class PawKeyApplication : Application() {
 
     private fun setDarkMode() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache"))
+                    .maxSizeBytes(50 * 1024 * 1024)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .respectCacheHeaders(false)
+            .build()
     }
 }
