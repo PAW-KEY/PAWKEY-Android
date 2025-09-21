26f7d072562d05fd4f9d95660cec4d10b2a75cb7
sonms <smsms5676@naver.com>
2025-09-21 17:35:50 +0900

    feat/#137 예전 버튼의 내용은 두고 새로운 공통 컴포넌트 버튼 구현 - pawkey 버튼은 나중에 제거예정

---
+++ app/src/main/java/com/paw/key/core/designsystem/component/DogkyButton.kt
@@ -0,0 +1,62 @@
+package com.paw.key.core.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.paw.key.core.designsystem.theme.PawKeyTheme
+import com.paw.key.core.extension.noRippleClickable
+
+@Composable
+fun DogkyButton(
+    text: String,
+    enabled: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val backgroundColor = when {
+        enabled -> PawKeyTheme.colors.primary
+        else -> PawKeyTheme.colors.background1
+    }
+
+    val textColor = when {
+        enabled -> PawKeyTheme.colors.background1
+        else -> PawKeyTheme.colors.default
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(backgroundColor, shape = RoundedCornerShape(8.dp))
+            .noRippleClickable {
+                if (enabled) onClick()
+            }
+            .padding(vertical = 14.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            style = PawKeyTheme.typography.mainButtonDefault,
+            color = textColor
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun DogkyButtonPreview() {
+    PawKeyTheme {
+        DogkyButton(
+            text = "",
+            enabled = true,
+            onClick = {}
+        )
+    }
+}
