ce2f898468d7ca7f3d3416a15bfffd0a8d8e3e0f
Son Juwan <wndhks171@naver.com>
2025-12-13 14:07:15 +0900

    mod/#155: mypage/main model 추가

--- app/src/main/java/com/paw/key/presentation/ui/mypage/main/model/MyPageContract.kt
+++ app/src/main/java/com/paw/key/presentation/ui/mypage/main/model/MyPageContract.kt
@@ -1,5 +1,6 @@
-package com.paw.key.presentation.ui.mypage.state
+package com.paw.key.presentation.ui.mypage.main.model
 
+import android.net.Uri
 import androidx.compose.runtime.Immutable
 
     @Immutable
@@ -8,7 +9,7 @@ import androidx.compose.runtime.Immutable
         val petName: String = "포비",
         val petAge: String = "12세",
         val petGender: String = "여아",
-        val petImageUrl: String = "",
+        val petImageUrl: Uri ?= null,
         val petTags: List<String> = listOf("조금 느긋해요", "#오토바이소리", "#대형견"),
         val walkCount: Int = 0,
         val totalDistance: String = "14km"
