4ccdb2789450dc9736c5c0b0fa9a0c05ae3b897f
sonms <smsms5676@naver.com>
2026-02-16 01:52:12 +0900

    feat/#154 루트 item용 공통 model 구현

--- app/src/main/java/com/paw/key/core/model/WalkingRouteUiModel.kt
+++ app/src/main/java/com/paw/key/core/model/WalkingRouteUiModel.kt
@@ -1,4 +1,4 @@
-package com.paw.key.presentation.ui.home.model
+package com.paw.key.core.model
 
 data class WalkingRouteUiModel(
     val id: Int,
@@ -16,8 +16,8 @@ data class WalkingRouteUiModel(
                 title = "한강 반포공원 코스",
                 distance = "3.2km",
                 time = "45",
-                date = "2025/11/06",
-                imageUri = "https://picsum.photos/400/300?random=1",
+                date = "25/11/06",
+                imageUri = "https://picsum.photos/300/400?random=1",
                 location = "서울 서초구"
             ),
             WalkingRouteUiModel(
@@ -25,8 +25,8 @@ data class WalkingRouteUiModel(
                 title = "북서울 꿈의숲 코스",
                 distance = "2.8km",
                 time = "38",
-                date = "2025/11/06",
-                imageUri = "https://picsum.photos/400/300?random=2",
+                date = "25/11/06",
+                imageUri = "https://picsum.photos/300/400?random=2",
                 location = "서울 강북구"
             ),
             WalkingRouteUiModel(
@@ -34,8 +34,8 @@ data class WalkingRouteUiModel(
                 title = "성수동 카페거리 산책로",
                 distance = "1.6km",
                 time = "25",
-                date = "2025/11/06",
-                imageUri = "https://picsum.photos/400/300?random=3",
+                date = "25/11/06",
+                imageUri = "https://picsum.photos/300/400?random=3",
                 location = "서울 성동구"
             ),
             WalkingRouteUiModel(
@@ -43,8 +43,8 @@ data class WalkingRouteUiModel(
                 title = "올림픽공원 호수길",
                 distance = "4.5km",
                 time = "60",
-                date = "2025/11/06",
-                imageUri = "https://picsum.photos/400/300?random=4",
+                date = "25/11/06",
+                imageUri = "https://picsum.photos/300/400?random=4",
                 location = "서울 송파구"
             ),
             WalkingRouteUiModel(
@@ -52,8 +52,8 @@ data class WalkingRouteUiModel(
                 title = "남산 둘레길",
                 distance = "5.2km",
                 time = "75",
-                date = "2025/11/06",
-                imageUri = "https://picsum.photos/400/300?random=5",
+                date = "25/11/06",
+                imageUri = "https://picsum.photos/300/400?random=5",
                 location = "서울 중구"
             )
         )
