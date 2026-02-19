62339bf514bc1ced84cb015a69a660d2429e2ebe
sonms <smsms5676@naver.com>
2026-02-19 23:57:28 +0900

    delete/#154 사용하지 않는 파일제거

--- app/src/main/java/com/paw/key/domain/model/entity/DummyUser.kt
+++
@@ -1,8 +0,0 @@
-package com.paw.key.domain.model.entity
-
-data class DummyUser(
-    val profile: String,
-    val firstName: String,
-    val id: Int,
-    val lastName: String,
-)
--- app/src/main/java/com/paw/key/domain/repository/onboarding/OnboardingRegionRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.paw.key.domain.repository.onboarding
-
-import DistrictResponse
-
-interface OnboardingRegionRepository {
-    suspend fun getOnboardingRegion(userId: Int): Result<DistrictResponse>
-}
