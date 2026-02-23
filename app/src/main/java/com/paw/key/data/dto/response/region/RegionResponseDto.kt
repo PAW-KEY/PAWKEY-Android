1f0cbc0a1a3ec7ed879e309d3f9fd6e5ca62453f
minseong-PC <smsdia412@gmail.com>
2026-02-23 17:54:05 +0900

    mod/#162 mapper 제거 후 데이터 클래스의 책임으로 변경

--- app/src/main/java/com/paw/key/data/dto/response/region/RegionResponseDto.kt
+++ app/src/main/java/com/paw/key/data/dto/response/region/RegionResponseDto.kt
@@ -1,5 +1,7 @@
 package com.paw.key.data.dto.response.region
 
+import com.paw.key.domain.entity.region.GeometryEntity
+import com.paw.key.domain.entity.region.RegionDataEntity
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -11,7 +13,15 @@ data class RegionResponseDto(
     val preRegionName: String,
     @SerialName("geometryDto")
     val geometryDto: GeometryDto
-)
+) {
+    fun toEntity(): RegionDataEntity {
+        return RegionDataEntity(
+            regionName = this.regionName,
+            preRegionName = this.preRegionName,
+            geometry = this.geometryDto.toEntity()
+        )
+    }
+}
 
 @Serializable
 data class GeometryDto(
@@ -19,4 +29,18 @@ data class GeometryDto(
     val type: String,
     @SerialName("coordinates")
     val coordinates: List<List<List<List<Double>>>>
-)
+) {
+    fun toEntity(): GeometryEntity {
+        return GeometryEntity(
+            type = this.type,
+            coordinates = this.coordinates.map { polygon ->
+                polygon.map { ring ->
+                    ring.map { point ->
+                        // 서버에서 위도 경도 다름
+                        Pair(point[1], point[0])
+                    }
+                }
+            }
+        )
+    }
+}
