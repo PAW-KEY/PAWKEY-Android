2fac541dedea66b6bee1220e03a12f067894688a
sonms <smsms5676@naver.com>
2026-03-05 14:20:01 +0900

    mod/#162 펫 프로필 model , entity, dto 수정

--- app/src/main/java/com/paw/key/data/dto/response/petprofile/PetProfileResponseDto.kt
+++ app/src/main/java/com/paw/key/data/dto/response/petprofile/PetProfileResponseDto.kt
@@ -1,8 +1,6 @@
 package com.paw.key.data.dto.response.petprofile
 
-import androidx.core.net.toUri
 import com.paw.key.domain.entity.petprofile.PetProfileEntity
-import com.paw.key.domain.entity.petprofile.TraitEntity
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -10,58 +8,35 @@ import kotlinx.serialization.Serializable
 data class PetProfileResponseDto(
     @SerialName("petId")
     val petId: Long,
-
-    @SerialName("name")
-    val name: String,
-
-    @SerialName("gender")
-    val gender: String,
-
-    @SerialName("isNeutered")
-    val isNeutered: Boolean,
-
-    @SerialName("age")
-    val age: Int,
-
-    @SerialName("isAgeKnown")
-    val isAgeKnown: Boolean,
-
-    @SerialName("breed")
-    val breed: String,
-
     @SerialName("imageUrl")
     val imageUrl: String,
-
-    @SerialName("traits")
-    val traits: List<TraitDto>,
-
-    @SerialName("walkCount")
-    val walkCount: Int,
+    @SerialName("name")
+    val name: String,
+    @SerialName("birth")
+    val birth: String,
+    @SerialName("age")
+    val age: Int,
+    @SerialName("gender")
+    val gender: String,
+    @SerialName("isNeutered")
+    val isNeutered: Boolean,
+    @SerialName("breed")
+    val breed: String,
+    @SerialName("dbtiName")
+    val dbtiName: String,
+    @SerialName("dbtiDescription")
+    val dbtiDescription: String
 ) {
     fun toEntity() = PetProfileEntity(
         petId = petId,
+        imageUrl = imageUrl,
         name = name,
+        birth = birth,
+        age = age,
         gender = gender,
         isNeutered = isNeutered,
-        age = age,
-        isAgeKnown = isAgeKnown,
         breed = breed,
-        imageUrl = imageUrl.toUri(),
-        walkCount = walkCount,
-        traits = traits.map { it.toEntity() }
-    )
-}
-
-@Serializable
-data class TraitDto(
-    @SerialName("category")
-    val category: String,
-
-    @SerialName("option")
-    val option: String,
-) {
-    fun toEntity() = TraitEntity(
-        category = category,
-        option = option
+        dbtiName = dbtiName,
+        dbtiDescription = dbtiDescription
     )
 }
---
+++ app/src/main/java/com/paw/key/presentation/ui/mypage/model/PetInfoModel.kt
@@ -0,0 +1,29 @@
+package com.paw.key.presentation.ui.mypage.model
+
+import com.paw.key.domain.entity.petprofile.PetProfileEntity
+
+data class PetInfoModel(
+    val petId: Int = -1,
+    val petImageUrl: String = "",
+    val petName: String = "",
+    val petBirthday: String = "",
+    val petGender: String = "",
+    val petBreed: String = "",
+    val petAge: Int = -1,
+    val petNeutered: Boolean = false,
+    val petDbtiName: String = "",
+    val petDbtiDescription: String = ""
+)
+
+fun PetProfileEntity.toUiModel() = PetInfoModel(
+    petId = petId.toInt(),
+    petImageUrl = imageUrl,
+    petName = name,
+    petBirthday = birth,
+    petGender = gender,
+    petBreed = breed,
+    petAge = age,
+    petNeutered = isNeutered,
+    petDbtiName = dbtiName,
+    petDbtiDescription = dbtiDescription
+)
