aaf20aa0bcd161a13a2245e734c4dde3c25fa800
sonms <smsms5676@naver.com>
2026-03-20 09:22:37 +0900

    chore/#162 사용하지 않는 레포지토리, DTO, 엔티티 및 리소스 삭제

--- app/src/main/java/com/paw/key/domain/repository/home/HomeRegionRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.paw.key.domain.repository.home
-
-import com.paw.key.domain.entity.home.HomeRegionDataEntity
-
-interface HomeRegionRepository {
-    suspend fun patchRegion(userId: Int, regionId: Int): Result<HomeRegionDataEntity>
-}
