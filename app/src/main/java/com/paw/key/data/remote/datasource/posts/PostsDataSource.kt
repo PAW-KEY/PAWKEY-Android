6e17246ca346f3ff67dc9a6f253b80805a376cd5
minseong-PC <smsdia412@gmail.com>
2026-03-20 17:50:14 +0900

    feat: PostsDataSource 내 게시글 작성, 상세 조회, 필터링 및 좋아요 API 연동

--- app/src/main/java/com/paw/key/data/remote/datasource/posts/PostsDataSource.kt
+++ app/src/main/java/com/paw/key/data/remote/datasource/posts/PostsDataSource.kt
@@ -1,14 +1,34 @@
 package com.paw.key.data.remote.datasource.posts
 
-import com.paw.key.data.dto.request.posts.PostsListRequestDto
+import com.paw.key.data.dto.request.posts.PostsDataRequestDto
+import com.paw.key.data.dto.request.posts.PostsFilterRequestDto
 import com.paw.key.data.service.posts.PostsService
 import javax.inject.Inject
 
 class PostsDataSource @Inject constructor(
     private val service: PostsService
 ) {
-    suspend fun postList(userId: Int, request: PostsListRequestDto) =
-        { }
+    suspend fun postPosts(request: PostsDataRequestDto) = service.postPosts(request)
 
-    suspend fun getAllPosts(userId: Int) = {}
+    suspend fun getCategories() = service.getCategories()
+
+    suspend fun getPostsDetail(postId: Int) = service.getPostsDetail(postId)
+
+    suspend fun getTop3Reviews(routeId: Int) = service.getTop3Reviews(routeId)
+
+    suspend fun getPostsFilter(
+        sortBy: String = "latest",
+        cursor: String? = null,
+        size: Int = 10,
+        request: PostsFilterRequestDto
+    ) = service.getPostsFilter(
+        sortBy = sortBy,
+        cursor = cursor,
+        size = size,
+        request = request
+    )
+
+    suspend fun getCategoriesFilter() = service.getCategoriesFilter()
+
+    suspend fun postLike(postId: Int) = service.postLike(postId)
 }
