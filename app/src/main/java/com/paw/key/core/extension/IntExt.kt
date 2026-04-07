57fe01530182389ed9c50936d6f915933458cce0
sonms <smsms5676@naver.com>
2026-04-07 17:42:50 +0900

    mod/image-type: 이미지 타입명 수정 및 UI/로직 개선
    - ImageDomainType의 WALK_POST를 WALK로 변경
    - Int.toTimeFormat 확장 함수 내 밀리초 단위를 초 단위로 변환하는 로직 추가
    - ImageTypeSelectDialog의 레이아웃 위치를 중앙에서 하단으로 조정 및 패딩 수정

--- app/src/main/java/com/paw/key/core/extension/IntExt.kt
+++ app/src/main/java/com/paw/key/core/extension/IntExt.kt
@@ -1,8 +1,9 @@
 package com.paw.key.core.extension
 
 fun Int.toTimeFormat(): String {
-    val hours = this / 3600
-    val minutes = (this % 3600) / 60
-    val seconds = this % 60
+    val totalSeconds = this / 1000
+    val hours = totalSeconds / 3600
+    val minutes = (totalSeconds % 3600) / 60
+    val seconds = totalSeconds % 60
     return "%02d:%02d:%02d".format(hours, minutes, seconds)
 }
