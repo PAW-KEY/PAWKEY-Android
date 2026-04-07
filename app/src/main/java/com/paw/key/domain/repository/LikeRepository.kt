7e16f9d82824d161d949d2a4c71747bae728b31d
sonms <smsms5676@naver.com>
2026-04-07 17:43:08 +0900

    refactor/#160: 좋아요 기능 관련 아키텍처 리팩터링
    - 기존 `LikeDataSource`, `LikeService`, `LikeRepository`, `LikeRepositoryImpl` 삭제
    - `LikeEntity` 및 `LikeResponseDto` 새로 정의하여 데이터 구조 재설계

--- app/src/main/java/com/paw/key/domain/repository/LikeRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.paw.key.domain.repository
-
-interface LikeRepository {
-    suspend fun likeCourse(userId: Int, postId: Int): Result<Unit>
-    suspend fun unlikeCourse(userId: Int, postId: Int): Result<Unit>
-}
