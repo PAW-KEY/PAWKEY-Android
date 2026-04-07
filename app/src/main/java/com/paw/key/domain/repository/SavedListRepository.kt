f7d301c256f8e817450ab7bf7ac04f050e4369b3
sonms <smsms5676@naver.com>
2026-04-07 17:41:21 +0900

    chore: SavedList 관련 데이터 및 도메인 레이어 삭제
    - 삭제: SavedListDataSource, SavedListRepository, SavedListRepositoryImpl, SavedListResponseDto, SavedListService

--- app/src/main/java/com/paw/key/domain/repository/SavedListRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.paw.key.domain.repository
-
-import com.paw.key.domain.entity.savedlist.SavedListPostEntity
-
-interface SavedListRepository {
-    suspend fun getSavedList(userId: Int): Result<SavedListPostEntity>
-}
